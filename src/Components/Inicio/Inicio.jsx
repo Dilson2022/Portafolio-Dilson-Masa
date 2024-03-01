f02a5ce9c673fe3e879b6e7a1f689a81ef8f2af1
Dilson Masa <Dilsonmasacontreras82@gmail.com>
2024-02-29 22:02:30 -0500

    actualizacion de portafolio

--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -4,14 +4,29 @@ function Inicio() {
   return (
     <div>
       <NavBar />
+      
       <h1>Frontend developers</h1>
+
+
+      <div className="Padre">
       <p className="inicio">
-        
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita,
-        deleniti. Nemo sed mollitia excepturi omnis culpa, dignissimos sapiente
-        repudiandae porro necessitatibus est dolorum cupiditate hic accusantium
-        iste ab veritatis iusto!.
+        Soy desarrollador full Stack, con mas de un año de experiencia, en desarrollo backend y frontend junior,
+        soy una persona proactiva y creativa. Me gusta trabajar en equipo y dar soluciones para optimizar las tareas.
+        Capacitado en gestion de proyectos.. 
       </p>
+      </div>
+
+
+      <nav className="redes">
+      <a target="blank" href="https://www.linkedin.com/in/dilson-masa-contreras-b068b4216/" className="icons-contacto" ><i className="fa-brands fa-linkedin" ></i></a>
+      <a target="blank" href="https://www.facebook.com/dilson.masacontreras?locale=es_LA" className="icons-contacto"><i className="fa-brands fa-facebook"></i></a>
+      <a target="blank" href="https://github.com/Dilson2022" className="icons-contacto"><i className="fa-brands fa-github"></i></a>
+      </nav>
+      
+
+      
+      
+
     </div>
   );
 }
